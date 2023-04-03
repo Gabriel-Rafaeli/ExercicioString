fef87ca62c7f0be930c295e2e782125fbac8f566
JV <JV@DESKTOP-8U4EPCU>
2023-04-03 10:16:50 -0300

    Add project files.

---
+++ ExercicioString01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExercicioString01.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase: \n");
+            string frase = Console.ReadLine();
+            Console.WriteLine();
+
+            string[] palavras = frase.Split(' ');
+            for (int i = 0; i < palavras.Length; i++)
+            {
+                palavras[i] = palavras[i].Substring(0, 1).ToUpper() + palavras[i].Substring(1);
+            }
+
+            string novaFrase = string.Join(" ", palavras);
+
+            Console.WriteLine("Frase com as primeiras letras maiúsculas: \n");
+            Console.WriteLine(novaFrase);
+
+        } 
+    }
+}
